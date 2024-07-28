3032c5bf6e4de5b5acb5be3d8ee94acbe4914d56
easttemplar <easttem@gmail.com>
2024-07-28 20:45:05 +0300

    Non-string solution

--- solution.cpp
+++ solution.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
-bool isPalindrome(int x) { return false; }
 
-int main() { return 0; }
+int input = 1442;
+
+bool isPalindrome(int x) {
+  if (x < 0) {
+    return false;
+  }
+
+  if (x != 0 && x % 10 == 0) {
+    return false;
+  }
+
+  int reverse = 0;
+
+  while (x > reverse) {
+    reverse = reverse * 10 + x % 10;
+    x /= 10;
+  }
+
+  return (x == reverse) || (x == reverse / 10);
+}
+
+int main() {
+  bool result = isPalindrome(input);
+  std::cout << "Result: " << result;
+  return 0;
+}
